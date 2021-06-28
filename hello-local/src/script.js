37c512413abbb4610cc00f3cb80ad5097013fe55
Noah Moats <85518918+noahmoats@users.noreply.github.com>
2021-06-28 12:10:30 -0500

    Add files via upload

---
+++ hello-local/src/script.js
@@ -0,0 +1,12 @@
+//jQuery
+$(document).ready(function () {
+  console.log("Hello, CodePen!");
+  //JavaScript
+  // Select the element and store it in a variable.
+  var el = document.getElementById("my-greeting");
+  // Change the value of the class attribute.
+  el.innerText = "Hello, Local! (programtically.)";
+
+  //let's do it again with jQ
+  $("#my-greeting").text("hello again");
+});
